1a6e7dace90471a45c151197c5ed59ad9299077e
jonathandoctor <jonathandoctor@gmail.com>
2011-08-27 13:17:11 +0000

    Adding media and static folders. Also templates. Basic changes to settings.py to accommodate these folders.

--- src/smalan/settings.py
+++ src/smalan/settings.py
@@ -1,5 +1,10 @@
 # Django settings for smalan project.
 
+import os
+import os.path
+PROJECT_WD = os.path.dirname(__file__)
+print 'Project location:', PROJECT_WD
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -39,28 +44,37 @@ SITE_ID = 1
 # to load the internationalization machinery.
 USE_I18N = True
 
+# Lang support - extra added by JD - perhaps we dont need..
+gettext = lambda s: s
+LANGUAGES = (
+    ('is', gettext('Icelandic')),
+)
+
+
 # If you set this to False, Django will not format dates, numbers and
 # calendars according to the current locale
 USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(PROJECT_WD, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = ''
+MEDIA_URL = 'http://localhost:8000/media/' # TODO: fix
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_WD, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
+STATIC_URL = 'http://localhost:8000/static/' # TODO: fix
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
