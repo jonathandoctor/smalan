b0717b5afaf5ba62b47d2d25d39797bf1e914cfc
jonathandoctor <jonathandoctor@gmail.com>
2011-08-27 14:15:16 +0000

    Adding urls and settings

--- src/smalan/settings.py
+++ src/smalan/settings.py
@@ -120,6 +120,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_WD, 'templates'),
 )
 
 INSTALLED_APPS = (
--- src/smalan/urls.py
+++ src/smalan/urls.py
@@ -1,17 +1,33 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'smalan.views.home', name='home'),
-    # url(r'^smalan/', include('smalan.foo.urls')),
+    # Main app
+    (r'^', include('smalan.main.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+# Add static files (temp)
+urlpatterns += patterns('',
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.STATIC_ROOT, 'show_indexes': True}),
+
+)
+
+# Add django admin and docs..
+urlpatterns += patterns('',
+    # Uncomment the admin/doc line below to enable admin documentation:
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    (r'^admin/', include(admin.site.urls)),
 )
